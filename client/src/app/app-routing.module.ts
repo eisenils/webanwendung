2955e4781e5936c387b8b5d4da20aeb53480b313
eisenils <mail.eisenils@gmail.com>
2019-10-31 13:21:56 +0100

    basic build up of the frontend, added bulma and a task-view.component

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
+import { TaskViewComponent } from './pages/task-view/task-view.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: TaskViewComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
